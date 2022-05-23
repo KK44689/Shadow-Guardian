4cb9431eb06a6ce9814cdc46f1d6546389649b16
KK44689 <61814979+KK44689@users.noreply.github.com>
2022-05-23 16:04:39 +0700

    add loop BG

---
+++ Assets/Scripts/LoopBG.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoopBG : MonoBehaviour
+{
+    private Vector3 startPos;
+
+    private float repeatWidth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+        repeatWidth = GetComponent<BoxCollider2D>().size.x / 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log (repeatWidth);
+        if (transform.position.x < -8.5f)
+        {
+            transform.position = startPos;
+        }
+    }
+}
