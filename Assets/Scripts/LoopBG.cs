1a3757890aa5c053cccb460b60e837970cdf2083
KK44689 <61814979+KK44689@users.noreply.github.com>
2022-05-26 17:03:13 +0700

    add sprites to game

--- Assets/Scripts/LoopBG.cs
+++ Assets/Scripts/LoopBG.cs
@@ -6,19 +6,38 @@ public class LoopBG : MonoBehaviour
 {
     private Vector3 startPos;
 
+    private float length;
+
     private float repeatWidth;
+
+    [SerializeField]
+    private float parallaxEffect;
 
     // Start is called before the first frame update
     void Start()
     {
         startPos = transform.position;
-        repeatWidth = GetComponent<BoxCollider2D>().size.x / 2;
+        length = GetComponent<SpriteRenderer>().bounds.size.x / 2;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.x < -8.5f)
+        // float temp = (transform.position.x * (1 - parallaxEffect));
+        // float dist = (transform.position.x * parallaxEffect);
+        // transform.position =
+        //     new Vector3(startPos + dist,
+        //         transform.position.y,
+        //         transform.position.z);
+        // if (temp > startPos + length)
+        // {
+        //     startPos += length;
+        // }
+        // else if (temp < startPos - length)
+        // {
+        //     startPos -= length;
+        // }
+        if (transform.position.x < startPos.x - length)
         {
             transform.position = startPos;
         }
