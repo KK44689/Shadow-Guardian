f3a1fb3331a594c582f209f4974bd3c01438e73c
KK44689 <61814979+KK44689@users.noreply.github.com>
2022-06-29 17:07:51 +0700

    add click to change on tutorial

--- Assets/Scripts/tutorial.cs
+++ Assets/Scripts/tutorial.cs
@@ -12,6 +12,10 @@ public class tutorial : MonoBehaviour
     // scene id
     private int mainSceneId = 1;
 
+    public GameObject clickText;
+
+    public GameObject tapText;
+
     private void Start()
     {
         tutorials[0].SetActive(true);
@@ -19,16 +23,45 @@ public class tutorial : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && index < tutorials.Length)
+        if (CheckDevice() == "Desktop")
+        {
+            clickText.SetActive(true);
+            tapText.SetActive(false);
+        }
+        if (CheckDevice() == "Handheld")
+        {
+            clickText.SetActive(false);
+            tapText.SetActive(true);
+        }
+        if ((Input.GetMouseButtonDown(0)) && (index < tutorials.Length))
         {
             tutorials[0].SetActive(false);
             tutorials[index - 1].SetActive(false);
             tutorials[index].SetActive(true);
             index++;
         }
-        else if (Input.GetKeyDown(KeyCode.Space) && index >= tutorials.Length)
+        else if ((Input.GetMouseButtonDown(0)) && index >= tutorials.Length)
         {
             SceneManager.LoadScene (mainSceneId);
+        }
+    }
+
+    private string CheckDevice()
+    {
+        //Check if the device running this is a desktop
+        if (SystemInfo.deviceType == DeviceType.Desktop)
+        {
+            return "Desktop";
+        }
+
+        //Check if the device running this is a handheld
+        if (SystemInfo.deviceType == DeviceType.Handheld)
+        {
+            return "Handheld";
+        }
+        else
+        {
+            return "Error";
         }
     }
 }
